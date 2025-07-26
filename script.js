d524e04c0f0a2fd8170ceb20d10d75e6a845375e
Julius Evan C. Estonilo <116896327+7equinox@users.noreply.github.com>
2025-07-26 21:25:37 +0800

    Write code to get the computer choice'

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log("Hello World");
+// console.log("Hello World");
+
+// Function to play computer's choice
+function getComputerChoice()
+{
+    let strCompChoice = "";
+
+    // Create random number for the condition/s below
+    const fltRndmNum = Math.random() * 100;
+
+    // Check the float number for testing the function
+    // console.log(fltRndmNum);
+
+    // if between 0 to 33.33, play "rock"
+    if (fltRndmNum <= 33.33)
+    {
+        strCompChoice = "rock";
+    }
+
+    // if between 33.34 to 66.67, play "paper"
+    else if (fltRndmNum <= 66.67)
+    {
+        strCompChoice = "paper";
+    }
+
+    // if between 66.68 to 100, play "scissors"
+    else
+    {
+        strCompChoice = "scissors";
+    }
+
+    return strCompChoice;
+}
+
+// test getComputerChoice()'s return
+// console.log(getComputerChoice());
+// console.log(getComputerChoice());
+// console.log(getComputerChoice());
+// console.log(getComputerChoice());
+// console.log(getComputerChoice());
